12bbefa66809ed9bd0ee5c4bcaa1423417cca931
Ebenezer Eshetie <77492217+EbenGitHub@users.noreply.github.com>
2023-03-10 04:28:21 +0300

    mn c

--- server/config.js
+++ server/config.js
@@ -4,9 +4,9 @@ const MODE = process.env.NODE_ENV
 
 const MONGODB_URI = (MODE === 'production') ? 
         process.env.MONGODB_URI : 
-    MODE === 'development' ? 
+    (MODE === 'development') ? 
         process.env.MONGODB_URI_DEV :
-    MODE === 'test' ? 
+    (MODE === 'test') ? 
         process.env.MONGODB_URI_TES : null
 
 const PORT = parseInt(process.env.PORT) || 4000
